a93e2560c75f18881cf4ebb9df8c9bf8b33cd8ec
g.pipart <g.pipart@student.isartdigital.com>
2025-06-29 17:15:07 +0200

    [Add] add spring arm and jump

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     [Header("References")]
     [SerializeField] private Rigidbody rigidbody;
     [SerializeField] private Camera camera;
+    [SerializeField] private Transform groundCheckStartPoint;
     
     [Header("Input Actions")]
     [SerializeField] private InputActionReference movement;
@@ -18,6 +19,7 @@ public class PlayerController : MonoBehaviour
     [Header("Parameters")] 
     [SerializeField] private float movementSpeed;
     [SerializeField] private float runningSpeed;
+    [SerializeField] private float jumpStrength;
 
     public EPlayerMovementState PlayerMovementState { get; private set; }
     private Vector3 _velocity;
@@ -30,7 +32,24 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
+        
         CatchInputs();
+    }
+
+    private void GroundCheck()
+    {
+        float maxDistance = 0.2f;
+
+        RaycastHit hit;
+        LayerMask layerMask = LayerMask.GetMask("Default");
+        if (Physics.Raycast(groundCheckStartPoint.position, -transform.up, out hit, maxDistance, layerMask))
+        {
+            PlayerMovementState = EPlayerMovementState.OnGround;
+        }
+        else
+        {
+            PlayerMovementState = EPlayerMovementState.InAir;
+        }
     }
 
     #region InputHandling
@@ -38,6 +57,7 @@ public class PlayerController : MonoBehaviour
     private void CatchInputs()
     {
         ApplyMovement(movement.action.ReadValue<Vector2>());
+        if (jump.action.IsPressed()) ApplySpaceBar();
     }
     
     private void ApplyMovement(Vector2 input)
@@ -48,8 +68,25 @@ public class PlayerController : MonoBehaviour
 
         float angle = Vector3.Angle(Vector3.forward, cameraForward);
         bool isRight = Vector3.Dot(cameraForward, Vector3.right) > 0;
-        
-        _velocity = (Quaternion.AngleAxis(isRight ? angle : -angle, Vector3.up) * new Vector3(input.x, 0, input.y)).normalized * movementSpeed;
+
+        switch (PlayerMovementState)
+        {
+            case EPlayerMovementState.OnGround:
+                _velocity = (Quaternion.AngleAxis(isRight ? angle : -angle, Vector3.up) * new Vector3(input.x, 0, input.y)).normalized * movementSpeed;
+                break;
+            
+            case EPlayerMovementState.InAir:
+                _velocity = (Quaternion.AngleAxis(isRight ? angle : -angle, Vector3.up) * new Vector3(input.x, 0, input.y)).normalized * movementSpeed;
+                break;
+        }
+    }
+
+    private void ApplySpaceBar()
+    {
+        if (PlayerMovementState == EPlayerMovementState.OnGround)
+        {
+            _velocity.y = jumpStrength;
+        }
     }
 
     #endregion
@@ -58,10 +95,28 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 rbVelocity = rigidbody.linearVelocity;
-        rbVelocity.x = _velocity.x;
-        rbVelocity.z = _velocity.z;
+        GroundCheck();
+
+        switch (PlayerMovementState)
+        {
+            case EPlayerMovementState.OnGround:
+                rbVelocity.x = _velocity.x;
+                rbVelocity.z = _velocity.z;
+                if(_velocity.y != 0) rbVelocity.y = _velocity.y;
+                break;
+            
+            case EPlayerMovementState.InAir:
+                if(_velocity.x != 0) rbVelocity.x = _velocity.x;
+                if(_velocity.z != 0) rbVelocity.z = _velocity.z;
+                if(_velocity.y != 0) rbVelocity.y = _velocity.y;
+                break;
+            
+        }
+        
+        
         
         rigidbody.linearVelocity = rbVelocity;
+
     }
     
     public enum EPlayerMovementState
